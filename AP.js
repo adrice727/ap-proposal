6d704b7ba9d5f52eb064044917182ca17cb7e8db
Aaron Rice <adrice727@gmail.com>
2016-04-06 13:16:43 -0700

    Add comments.

--- AP.js
+++ AP.js
@@ -8,6 +8,50 @@
             this.session = options.session;
             this.setListeners(options.compListeners);
         }
+        
+        /** 
+         * Accepts event listeners for one or multiple components and combines them into a single
+         * object that can be passed to 'session.on()'.  When multiple components listen for the 
+         * same event, their event handlers will be combined into a single callback in which each
+         * is fired.
+         * 
+         * Example:
+         *  comp1Listeners = {
+         *      x: doComp1X(),
+         *      y: doComp1Y()
+         *  };
+         * 
+         *  comp2Listeners = {
+         *      x: doComp2X(),
+         *      y: doComp2Y(),
+         *      z: doComp2Z()
+         *  };
+         * 
+         *  comp3Listeners = {
+         *      z: doComp3Z()
+         *  };
+         * 
+         *  let finalListenrs = setListners(comp1Listeners, comp2Listeners, comp3Listeners)
+         * 
+         *  console.log(finalListenrs);
+         * 
+         *  {
+         *      x: function(){ 
+         *            doComp1X();
+         *            doComp2X();
+         *      },
+         *      y: function(){ 
+         *            doComp1Y();
+         *            doComp2Y();
+         *      },
+         *      z: function(){ 
+         *            doComp2Z();
+         *            doComp3Z();
+         *      }
+         *  }
+         * 
+         * @param {...Object} compListeners - A set of event listeners
+         */
 
         setListeners(...compListeners) {
 
