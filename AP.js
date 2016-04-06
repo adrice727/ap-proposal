5aab6f6ba8844ab6b1b6de6bc0ed8250209608af
Aaron Rice <adrice727@gmail.com>
2016-04-06 12:00:00 -0700

    AP can combine event handlers.

--- AP.js
+++ AP.js
@@ -10,14 +10,30 @@
         }
 
         setListeners(...compListeners) {
-            let listeners = compListeners.reduce((acc, l) => {
-                return Object.assign(acc, l);
+
+            let listeners = compListeners.reduce((outerAcc, listenerSet) => {
+
+                let combinedListener = Object.keys(listenerSet).reduce((innerAcc, event) => {
+
+                    let existingHandler = outerAcc[event];
+
+                    if (existingHandler) {
+                        innerAcc[event] = () => [existingHandler, listenerSet[event]].forEach(fn => fn());
+                    } else {
+                        innerAcc[event] = listenerSet[event];
+                    }
+
+                    return innerAcc;
+                }, {});
+
+                return Object.assign(outerAcc, combinedListener);
+
             }, {});
 
             this.session.on(listeners);
         }
-    }
-    
+    };
+
     this.AP = AP;
 
 })(this);
